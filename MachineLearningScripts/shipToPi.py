eadb12736ab398764be045340e3dc1cd2c9c07bc
Exevior86 <bucco1221@gmail.com>
2020-05-25 20:18:58 -0700

    Update shipToPi.py

--- MachineLearningScripts/shipToPi.py
+++ MachineLearningScripts/shipToPi.py
@@ -6,6 +6,9 @@ import numpy as np
 from sklearn.externals import joblib 
 import pickle
 
+# A script to test loading a machine learning model to the raspberry pi
+# to make predictions on the device after training the model.
+
 data = pd.read_csv('CombinedCsvWithGames')
 array = data.values
 x = array[:,0:5]
