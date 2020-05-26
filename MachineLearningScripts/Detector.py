d1eb555ee0bcec23dc8bf905213f530401529ba5
Exevior86 <bucco1221@gmail.com>
2020-05-25 20:17:34 -0700

    Update Detector.py

--- MachineLearningScripts/Detector.py
+++ MachineLearningScripts/Detector.py
@@ -4,6 +4,9 @@
 #import pandas as pd
 import numpy as np
 
+# Used on raspberry pi to predict data collected, a test to see
+# if the device could run it and at what cost.
+
 data = pd.read_csv('CombinedCsvyep.csv')
 filename = 'MalwareDetectionModel.sav'
 model = joblib.load(filename)
